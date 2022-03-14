78cde90a43e82b2a4bbbd4e6ed02d4dc1a34f446
ashley-mallia <Ashley.Mallia@icloud.com>
2022-03-14 17:30:33 +1100

    Initial commit

---
+++ tests/unit/model.todo.test.js
@@ -0,0 +1,41 @@
+const mockingoose = require('mockingoose')
+const Todo = require("../../models/Todo")
+
+describe('mockingoose', () => {
+    beforeEach(() => {
+        mockingoose.resetAll(),
+            jest.clearAllMocks()
+    })
+
+    describe("Explicit Tests", () => {
+        it('Should validate', async () => {
+            const todo = new Todo({
+                name: "Valid Name",
+                done: false
+            });
+
+            const result = await todo.validateSync()
+
+            expect(result).toBe(undefined)
+
+        });
+
+        it('Should find', async () => {
+
+            mockingoose(Todo).toReturn([{ name: "Task", done: false }]);
+
+            const result = await Todo.find().where('name').in('1');
+
+            expect(result).toHaveLength(1);
+        });
+
+        it('Should error when no name', async () => {
+            const todo = new Todo({ name: "", done: false });
+
+            const result = await todo.validateSync();
+
+            expect(result.errors['name'].message).toBe("Path `name` (``) is shorter than the minimum allowed length (1).");
+
+        })
+    })
+})
